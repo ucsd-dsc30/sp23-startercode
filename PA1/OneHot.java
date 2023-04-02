613f96198ae1091bd4cc8cffc01734527da276d0
Yunyi Huang <yuh022@ucsd.edu>
2023-04-01 18:08:22 -0700

    PA1 starter code

---
+++ PA1/OneHot.java
@@ -0,0 +1,21 @@
+public class OneHot {
+    // Part 1
+    public static String[] getUnique(String[] arr) {
+        //TODO
+        return null;
+    }
+
+
+    // Part 2
+    public static int getNumOfUniqueElements(String[] arr) {
+        //TODO
+        return 0;
+    }
+
+
+    // Part 3
+    public static int[][] oneHotEncode(String[] arr) {
+        //TODO
+        return null;
+    }
+}
