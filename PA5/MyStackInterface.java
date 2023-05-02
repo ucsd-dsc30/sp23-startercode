2822d6fbb016b11a05c80794a931313a963c701f
Brian96086 <brian96086@gmail.com>
2023-05-01 23:45:06 -0700

    push PA5 startercode

---
+++ PA5/MyStackInterface.java
@@ -0,0 +1,15 @@
+public interface MyStackInterface<T>  {
+
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public void push(T data);
+
+    public T pop();
+
+    public T peek();
+
+}
+
---
+++ PA5/WebBrowser.java
@@ -0,0 +1,78 @@
+/*
+ * NAME: TODO First Last
+ * PID: TODO Axxxxxxxx
+ */
+
+/**
+ * TODO
+ *
+ * @author TODO
+ * @since TODO
+ */
+public class WebBrowser {
+
+    private String currentPage;
+    private MyArrayList<String> history;
+    private MyStack<String> prev;
+    private MyStack<String> next;
+
+    private static final String DEFAULT_PAGE = "google.com";
+
+    /**
+     * TODO
+     */
+    public WebBrowser() {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public String getCurrentPage() {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public void openNewPage(String newLink) {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public void previousPage() {
+        // TODO: complete implementation
+
+    }
+
+    /**
+     * TODO
+     */
+      public void nextPage() {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public void newTab() {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public MyArrayList<String> getHistory() {
+        // TODO: complete implementation
+    }
+
+    /**
+     * TODO
+     */
+    public boolean findLink(String link) {
+       // TODO: complete implementation
+    }
+
+}
